af647c95b038e3264db404648cba231e229e55d0
Ian <biboyboy04@gmail.com>
2022-08-03 23:11:21 +0800

    Add functions at script.js and Delete a class at style.css - Wrap grid creation and hover effect codes into a function and update/improve them. - Add removeGrid and setGrid functions, and button eventlistener - Remove square class and use descendant selector instead

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-const container = document.querySelector('.container');
-
-for(let j = 0; j<16; j++){
-    for(let k = 0; k<16; k++){
+function createGrid(pixels = 16){
+    const container = document.querySelector('.square-container');
+    let dimensions = 100/pixels;
+    for(let k = 0; k<pixels**2; k++){
         const square = document.createElement('div');
-        square.classList.add('square');
+        square.style.width = `${dimensions}%`;
+        square.style.height = `${dimensions}%`;
+        square.style.backgroundColor = "black";
         container.appendChild(square);
     }
+    squareHover();
+};
+
+function squareHover(){
+    const squares = document.querySelectorAll('.square-container div');
+    squares.forEach(square => square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = "white";
+        })
+    );
+};
+
+const button = document.querySelector('button');
+button.addEventListener('click', setGrid); 
+
+function setGrid(){
+    let val = prompt('Pixel Grid: ','');
+    let valInt = parseInt(val);
+    if(valInt !== null && Number.isInteger(valInt)) {
+        removeGrid();
+        return createGrid(valInt);
+    }
+    else return;
 }
 
-container.appendChild(testP);
+function removeGrid(){
+    const container = document.querySelector('.square-container');
+    const squares = document.querySelectorAll('.square-container div');
+    squares.forEach(square => container.removeChild(square));
+}
+
+createGrid();
